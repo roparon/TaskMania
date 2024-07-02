2d8d4e55914b910dd06eef7e777fa2658761b851
Captain <enockbett427@gmail.com>
2024-07-02 09:32:42 +0300

    added routes

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/rop/Africode/TaskMania/env"
+set -gx VIRTUAL_ENV "/home/captain/Africode/TaskMania/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
